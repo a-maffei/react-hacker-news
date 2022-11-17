7097b04dc6f9939276d38a7b5fa3fb7c4c4dc360
oh-ve <maricic.ve@gmail.com>
2022-11-17 13:53:50 +0100

    our cool spinner

--- src/App.js
+++ src/App.js
@@ -3,27 +3,36 @@ import Header from "./components/Header";
 import {useState, useEffect} from 'react';
 import axios from "axios";
 import Articles from "./components/Articles";
+import { SpinnerDotted } from 'spinners-react'
 
 
 function App() {
   const [userInput, setUserInput] = useState('');
   const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+ 
   const url = `http://hn.algolia.com/api/v1/search?query=${userInput}&tags=story`
   
   const fetchData = () => {
-    axios.get(url).then((response) => {
+    setLoading(true);
+    const timer = setTimeout(() => axios.get(url).then((response) => {
       setArticles(response.data.hits);
-      console.log(response.data.hits)})}
+      setLoading(false);
+      console.log(response.data.hits)}), 3000);
+      return () => clearTimeout(timer)}
 
-    useEffect(() => {
-      fetchData();
-    }, [userInput]);
+    useEffect(() => fetchData(), [userInput]);
 
-  return (
-    <>
+  return loading ? (
+      <div id="loading">
+        <Header userInput={userInput} setUserInput={setUserInput}/>
+        <div id="spinner"><SpinnerDotted color="000000"/></div>
+      </div> 
+  ) : (
+    <div id="everything">
       <Header userInput={userInput} setUserInput={setUserInput}/>
       <Articles articles={articles}/>
-    </>
+    </div>
   );
 }
 
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 export default function Searchbar() {
+  
   return (
     <div>
       <input type="text" placeholder="Search.."></input>
