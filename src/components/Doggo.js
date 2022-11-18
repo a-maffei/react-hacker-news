0a6b5e7d7b3a52b63cb5c7f3e8c715ddf81203fa
oh-ve <maricic.ve@gmail.com>
2022-11-18 11:28:58 +0100

    doggo-generator working

--- src/components/Doggo.js
+++ src/components/Doggo.js
@@ -4,7 +4,7 @@ import lafee from "./lafee.jpg"
 import elio from "./elio.jpg"
 
 export default function Doggo() {
-    const [randomImage, setRandomImage] = useState('');
+    const [{image}, setRandomImage] = useState({image: ''});
    
     const renderImage = () => {
         const Images = [
@@ -12,17 +12,17 @@ export default function Doggo() {
             {image: lafee},   
             {image: elio}
         ];
-        const randomImageIndex = Math.floor(Math.random() * Math.floor(3));
+        const randomImageIndex = Math.floor(Math.random() * 3);
         return Images[randomImageIndex];
     };    
     
     useEffect(() => {
         setRandomImage(renderImage);
-    });
+    }, []);
 
     return (
         <div>
-            <img src={renderImage()}/>
+            <img src={image}/>
         </div>
     );
 }
